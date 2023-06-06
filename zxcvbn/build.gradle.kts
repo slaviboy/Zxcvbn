4a2222232292e105ab2bbf14d7a1071fb9091577
Stanislav Krasimirov Georgiev <slavi94slavi94@gmail.com>
2023-06-06 22:05:57 +0300

    Implement Kotlin DSL for module zxcvbn

---
+++ zxcvbn/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.library")
+    id("maven-publish")
+    kotlin("android")
+}
+
+android {
+    namespace = ApplicationConfiguration.libraryNamespace
+    compileSdk = ApplicationConfiguration.compileSdk
+    defaultConfig {
+        minSdk = ApplicationConfiguration.minSdk
+        targetSdk = ApplicationConfiguration.targetSdk
+        testInstrumentationRunner = ApplicationConfiguration.androidTestInstrumentation
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
+    }
+    kotlinOptions {
+        jvmTarget = "18"
+    }
+    android.sourceSets.getByName("main") {
+        res.srcDirs(
+            "/src/main/res/raw/matchers/dictionaries/res",
+            "/src/main/res/raw/matchers/keyboards/res"
+        )
+    }
+    testOptions.unitTests.isIncludeAndroidResources = true
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = ApplicationConfiguration.groupId
+                artifactId = ApplicationConfiguration.artifactId
+                version = ApplicationConfiguration.version
+            }
+        }
+    }
+}
+
+dependencies {
+    implementations(*ApplicationDependencies.dependenciesLibrary)
+    testImplementations(*ApplicationDependencies.dependenciesTest)
+}
